f23c0e977ad841a4237a165c8d18f1667a4deb6f
Marcelo Francisco <marceloffdeoliveira2@gmail.com>
2021-02-27 13:16:00 -0300

    Primeiro Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ExperienceBar from './components/ExperinceBar';
+
+import './styles/global.css';
+function App() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  );
+}
+
+export default App;
